aa76b3c8279d1d1cc66751f2ea88410aa88a9f99
Rishah Dwivedi <rishabh_19@live.com>
2018-06-05 12:33:11 +0530

    Add files via upload

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -95,8 +95,8 @@ public class DoublyLinkedList<E> {
     	 if(isEmpty())
     		 return null;
     	 Node<E> node = header;
-    	 for(int i=0; i<size;i++) {
-    		 node = header.getNext();
+    	 for(int i=-1; i<index;i++) {
+    		 node = node.getNext();
     	 }
     	 return node;
     	 
